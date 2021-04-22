3fc8142128bf859bf398e375315b01137d3c5062
Zontreck <tarapiccari@gmail.com>
2021-04-22 10:10:17 -0700

    test more changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,64 @@ function App() {
                 blur: 5
               }
             }
+          },
+          polygon: {
+            enable: true,
+            type: 'inside',
+            move:{
+              radius: 100
+            }
+          },
+          autoPlay: true,
+          interactivity: {
+            detectsOn: "window",
+            events:{
+              onClick: {
+                enable:true
+              }
+            }
+          },
+          particles:{
+            collisions:{
+              bounce:{
+                horizontal:{
+                  random:{
+                    enable: false,
+                    minimumValue: 0.1
+                  },
+                  value:1
+                },
+                vertical:{
+                  random:{
+                    enable:false,
+                    minimumValue:0.1
+                  },
+                  value:1
+                }
+              },
+              enable:true,
+              mode: "destroy",
+              overlap:{
+                enable:true,
+                retries:0
+              }
+            },
+            destroy:{
+              mode:'split',
+              split:{
+                count: 2,
+                rate:{
+                  random:{
+                    enable:true,
+                    minimumValue:1
+                  },
+                  value:{
+                    min: 1,
+                    max:10
+                  }
+                }
+              }
+            }
           }
         }}
         style={{
