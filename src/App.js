8a27d677987d5823ec9cf4b574b5d8882b65b56b
Zontreck <tarapiccari@gmail.com>
2021-04-22 09:41:57 -0700

    Commit new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Particles from "react-particles-js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles
+        params={{
+          particles: {
+            line_linked: {
+              shadow:{
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5
+              }
+            }
+          }
+        }}
+        style={{
+          width: '100%'
+        }}
+      
+      />
     </div>
   );
 }
